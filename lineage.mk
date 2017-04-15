b50b740574584e271af0e758ab9818ffa64243e3
ishantvivek <ishantvivek1311@gmail.com>
2017-04-15 07:56:47 +0000

    Welcome to LineageOS
    * RIP CM :'(

--- lineage.mk
+++ lineage.mk
@@ -6,7 +6,7 @@ $(call inherit-product, device/samsung/kyleprods/device_kyleprods.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := kyleprods
-PRODUCT_NAME := cm_kyleprods
+PRODUCT_NAME := lineage_kyleprods
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := GT-S7582
